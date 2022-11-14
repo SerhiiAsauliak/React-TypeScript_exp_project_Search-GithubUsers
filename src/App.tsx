f30960d3e46e6070b0e267f806281b961c813ba3
Serhii <mel.gipppson@gmail.com>
2022-11-14 22:07:51 +0200

    added search element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
 import { Container } from "./components/Container";
+import { Search } from "./components/Search";
 
 function App() {
   return (
     <Container>
       <Header/>
+      <Search hasError onSubmit={() => {}}/>
     </Container>
   );
 }
