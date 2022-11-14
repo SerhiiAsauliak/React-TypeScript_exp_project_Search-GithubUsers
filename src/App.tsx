4a90e8f12e4d034b4fffd20dbb41e6e2634670f5
Serhii <mel.gipppson@gmail.com>
2022-11-14 18:36:51 +0200

    added theme switcher logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header";
+import { Container } from "./components/Container";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Container>
+      <Header/>
+    </Container>
   );
 }
 
